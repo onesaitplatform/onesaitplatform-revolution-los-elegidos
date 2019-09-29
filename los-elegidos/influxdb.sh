03e8f794a9ef0ea83b372537b505d4b7e0e9ada2
Francisco Javier Ahijado <fjahijado@minsait.com>
2019-09-29 22:19:57 +0200

    Métricas técnicas

---
+++ los-elegidos/influxdb.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE "cadvisor"'
+curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE "prometheus"'
